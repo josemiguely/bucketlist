220645fdaa30238cd7eee8545eefbf6584da4ad0
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-01-04 13:06:43 -0300

    feat: add editing bucket with refactor to new
    It uses partials to reuse the forms

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,10 +1,11 @@
 class BucketsController < ApplicationController
+  before_action :set_bucket, only: %i[ show edit update]
+
   def index
     @buckets = Bucket.all
   end
 
   def show
-    @bucket = Bucket.find(params[:id])
   end
 
   def new
@@ -20,7 +21,22 @@ class BucketsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @bucket.update(bucket_params)
+      redirect_to @bucket
+    else
+      render :update, status: :unprocessable_entity
+    end
+  end
+
   private
+  def set_bucket
+    @bucket = Bucket.find(params[:id])
+  end
+
   def bucket_params
     params.expect(bucket: [ :title, :description ])
   end
