d2d1684e7c244be25c005f705b474f52ee787dc8
josemiguelywsl2 <josemiguelyuseff@gmail.com>
2025-01-04 12:51:55 -0300

    feat: add creation of buckets functionality

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -6,4 +6,22 @@ class BucketsController < ApplicationController
   def show
     @bucket = Bucket.find(params[:id])
   end
+
+  def new
+    @bucket = Bucket.new
+  end
+
+  def create
+    @bucket = Bucket.new(bucket_params)
+    if @bucket.save
+      redirect_to @bucket
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def bucket_params
+    params.expect(bucket: [ :title, :description ])
+  end
 end
